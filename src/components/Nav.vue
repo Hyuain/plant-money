2c61126bf8971882067c36d5b6c6e76c27428cae
Hyuain <Hyuain97@163.com>
2020-01-17 15:04:05 +0800

    home 页面布局

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,5 +1,11 @@
 <template>
-  <div></div>
+  <div id="nav">
+    <router-link to="/home">记账</router-link>
+    |
+    <router-link to="/labels">标签</router-link>
+    |
+    <router-link to="/statistics">统计</router-link>
+  </div>
 </template>
 
 <script>
@@ -9,5 +15,7 @@
 </script>
 
 <style lang="scss" scoped>
+  #nav{
 
+  }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>Home.vue
+  <div id="home">
+    <div class="main">
+      Home.vue
+    </div>
     <Nav/>
   </div>
 </template>
@@ -12,5 +15,14 @@
 </script>
 
 <style lang="scss" scoped>
-
+  #home{
+    display: flex;
+    flex-direction: column;
+    border: 1px solid green;
+    height: 100vh;
+    .main{
+      flex-grow: 1;
+      overflow: auto;
+    }
+  }
 </style>
