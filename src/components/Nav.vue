b39355a21aa930e264268a7a0295bd7509450a46
Hyuain <Hyuain97@163.com>
2020-01-17 15:57:14 +0800

    引入 svg

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,10 +1,17 @@
 <template>
   <div id="nav">
-    <router-link to="/home">记账</router-link>
+    <router-link to="/labels">
+      <Icon name="labels"/>
+      标签
+    </router-link>
     |
-    <router-link to="/labels">标签</router-link>
+    <router-link to="/home">
+      <Icon name="money"/>
+      记账</router-link>
     |
-    <router-link to="/statistics">统计</router-link>
+    <router-link to="/statistics">
+      <Icon name="statistics"/>
+      统计</router-link>
   </div>
 </template>
 
