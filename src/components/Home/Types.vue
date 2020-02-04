c02a840934fec18e299fa01a91afeac9ec60b3bf
Hyuain <Hyuain97@163.com>
2020-02-04 21:57:15 +0800

    模块化

---
+++ src/components/Home/Types.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul class="types">
+    <li class="selected">支出</li>
+    <li>收入</li>
+  </ul>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'Types'
+  };
+</script>
+
+<style lang="scss" scoped>
+  .types {
+    background: #c4c4c4;
+    display: flex;
+    text-align: center;
+    font-size: 24px;
+    li {
+      width: 50%;
+      line-height: 64px;
+      height: 64px;
+      &.selected {
+        border-bottom: 4px solid;
+      }
+    }
+  }
+
+</style>
