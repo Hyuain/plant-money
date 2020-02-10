115f160a9bdcbe1cad1b4270c70d8dec8fb8b605
Hyuain <Hyuain97@163.com>
2020-02-10 20:19:59 +0800

    新增 EditLabel 组件

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -8,10 +8,21 @@
 <script lang="ts">
   import Vue from 'vue';
   import {Component} from 'vue-property-decorator';
+  import labelsModel from '@/models/labelsModel';
 
   @Component
   export default class EditLabel extends Vue {
-
+    created() {
+      const {id} = this.$route.params;
+      labelsModel.fetch();
+      const labels = labelsModel.data;
+      const label = labels.filter(t => t.id === id)[0];
+      if(label){
+        console.log(label);
+      } else {
+        this.$router.replace('/404')
+      }
+    }
   }
 </script>
 
